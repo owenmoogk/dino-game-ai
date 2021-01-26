ca4f1e483ee39e4428c58cbbb751bdf193bf0978
owenmoogk <owenmoogk@gmail.com>
2021-01-25 21:56:45 -0500

    cleaned up a lot of the code

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 # owen moogk
-# ai flappy dino
-# jan 1 2020
+# ai dino game
 
-# imports
 import pygame, random, os, time, sys, pickle, keyboard
 from random import randint
 from pygame import mixer
@@ -43,8 +41,7 @@ font = pygame.font.SysFont("comicsans", 50)
 screen = pygame.display.set_mode((windowWidth, windowHeight))
 pygame.display.set_caption('Dino Game')
 
-# dino class
-class dino:
+class Dino:
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -55,36 +52,44 @@ class dino:
     def move(self):  
         self.ySpeed += gravity
         self.y += self.ySpeed
+        if self.y > windowHeight - dinoHeight:
+            self.y = windowHeight - dinoHeight
 
-# cactus class
-class cactus:
+class Cactus:
+    def __init__(self, y, x):
+        self.y = y
+        self.x = x
+    def move(self):
+        self.x -= cactusSpeed
+    def collision(self, dino):
+        collided = False
+        if self.x < dino.x + dinoWidth and self.x + cactusWidth > dino.x: # checking if the cactus x overlaps with the dino x
+            if self.y < dino.y + dinoHeight:
+                collided = True
+        return(collided)
+
+class Bird():
     def __init__(self, y,x):
         self.y = y
         self.x = x
 
 def getInputs():
-    # looping thru events
     events = pygame.event.get()
-    running = True
     for event in events:
         # if x button pressed stop just break out of these loops
         if event.type == pygame.QUIT:
-            running = False
-        # if key is pressed
-                        
-        if keyboard.is_pressed('space'):  # if key 'q' is pressed 
+            quit()      
+        if keyboard.is_pressed('space'):
             if d1.y >= windowHeight - dinoHeight:
                 d1.jump()
-    return(running)
 
 def restart(started):
-    global d1, cactuss, died, score, highScore, scoreDelay
-    d1 = dino(dinoX,windowHeight-dinoHeight)
-    cactuss = []
-    cactuss.append(cactus(windowHeight - cactusHeight,windowWidth))
-    died = True
+    global d1, cacti, died, score, highScore
+    d1 = Dino(dinoX,windowHeight-dinoHeight)
+    cacti = []
+    cacti.append(Cactus(windowHeight - cactusHeight,windowWidth))
+    died = False
     score = 0
-    scoreDelay = 0
     if started == False:
         if score > highScore:
             highScore = score
@@ -93,66 +98,42 @@ def restart(started):
         highScore = pickle.load(open("highscore.dat", "rb"))
         started = True
 
-running = True
-started = True
-
-restart(started)
-
-# main running loop
-while running:
-
-    if died == False:
-        restart(started)
-    
-    # background
+def renderScreen(dino, cacti):
     pygame.draw.rect(screen, backgroundColor, (0,0,windowWidth, windowHeight))
+    screen.blit(dinoImg,(dino.x,dino.y))
+    for cactus in cacti:
+        screen.blit(cactusImg,(cactus.x,cactus.y))
 
-    running = getInputs() # if return is false then user wants to quit; end while loop
-    d1.move()
-
-    # y collision detection
-    if d1.y < 0:
-        died = False
-    if d1.y > windowHeight - dinoHeight:
-        d1.y = windowHeight - dinoHeight
-
-    # if the forwardmost cactus is off screen then delete
-    if cactuss[len(cactuss)-1].x < windowWidth - 400:
-        cactuss.append(cactus(windowHeight - cactusHeight, windowWidth))
-
-    # loop thru cactuss
-    for i in cactuss:
-
-        # move cactuss
-        i.x -= cactusSpeed
-
-        # collision detection
-        if i.x < d1.x + dinoWidth and i.x + cactusWidth > d1.x: # checking if the cactus x overlaps with the dino x
-            if i.y < d1.y + dinoHeight:
-                died = False
-        
-        # blit images
-        screen.blit(cactusImg,(i.x,i.y))
-
-        # when off screen delete
-        if i.x < 0 - cactusWidth:
-            cactuss.remove(cactuss[0])
-
-    # score onto screen
+    # scores
     score_label = font.render("Score: " + str(score),1,(scoreColor))
     screen.blit(score_label, (10, 10))
     highScoreLabel = font.render("High Score: "+str(highScore),1,(scoreColor))
     screen.blit(highScoreLabel, (10, 50))
 
-    # dino onto screen
-    screen.blit(dinoImg,(d1.x,d1.y))
-    # score
-    if scoreDelay < 3:
-        scoreDelay += 1
-    else:
-        score += 1
-        scoreDelay = 0
+started = True
+restart(started)
 
-    # final update to the screen
+# main running loop
+while True:
+    if died == True:
+        restart(started)
+    if getInputs() == False:
+        quit()
+    d1.move()
+
+    # if the forward most cactus is off screen then delete
+    if cacti[len(cacti)-1].x < windowWidth - 400:
+        cacti.append(Cactus(windowHeight - cactusHeight, windowWidth))
+
+    for cactus in cacti:
+        cactus.move()
+        died = cactus.collision(d1)
+        if died:
+            break
+        if cactus.x < 0 - cactusWidth:
+            cacti.remove(cacti[0])
+
+    score += 1
+    renderScreen(d1, cacti)
     clock.tick(gameSpeed)
     pygame.display.update()
